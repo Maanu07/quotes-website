f40bd9b770b18481b2ccd719f37ab849261c9311
devil_07 <manasjoshi.mj6@gmail.com>
2021-07-12 08:26:06 +0530

    first local commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const btn = document.querySelector(".getQuote");
+const insertInto = document.querySelector(".quotes");
+const twitterBtn = document.getElementById("twitter-btn");
+
+let res, result;
+
+const getQuote = () => {
+  let quote = result[Math.floor(Math.random() * result.length)];
+  while (quote.text.length > 50) {
+    quote = result[Math.floor(Math.random() * result.length)];
+  }
+
+  let template = `
+      <h3 class='quote-text'>${quote.text}</h3>
+      <p class='author'>By ~ ${quote.author}</p>
+     `;
+
+  insertInto.innerHTML = template;
+};
+
+async function fetchQuotes() {
+  const api = "https://type.fit/api/quotes";
+
+  try {
+    res = await fetch(api);
+    result = await res.json();
+    getQuote();
+  } catch (err) {
+    alert(err);
+  }
+}
+
+const tweet = () => {
+  const tweetBody = document.querySelector(".quote-text").innerText;
+
+  //twitter web intent url 'https://twitter.com/intent/tweet'
+  let api = "https://twitter.com/intent/tweet";
+  window.open(
+    api + `?text=${tweetBody}&hashtags=motivation,workHard`,
+    "",
+    "width=400,height=200,top=center,left=center"
+  );
+};
+
+btn.addEventListener("click", getQuote);
+
+twitterBtn.addEventListener("click", tweet);
+
+fetchQuotes();
